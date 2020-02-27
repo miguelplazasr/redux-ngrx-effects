c5ee7a4a445c09cad640d4ef593bcc01f49f5c85
miguelplazasr <info@miguelplazasr.com>
2020-02-26 22:58:33 -0500

    145. Acciones de usuarios

---
+++ src/app/store/actions/usuarios.actions.ts
@@ -0,0 +1,28 @@
+
+// ToDo: 1. Definicion de las acciones
+import {Action} from '@ngrx/store';
+import {UsuarioModel} from '../../models/usuario.model';
+
+export const CARGAR_USUARIOS = '[Usuarios] Cargar usuarios';
+export const CARGAR_USUARIOS_FAIL = '[Usuarios] Cargar usuarios Fail';
+export const CARGAR_USUARIOS_SUCCESS = '[Usuarios] Cargar usuarios Success';
+
+// ToDO: 2. Cleacion de las clases que permitan crear acciones
+export class CargarUsuarios implements Action {
+  readonly type = CARGAR_USUARIOS;
+}
+
+export class CargarUsuariosFail implements Action {
+  readonly type = CARGAR_USUARIOS_FAIL;
+
+  constructor( public payload: any ) {}
+}
+
+export class CargarUsuariosSuccess implements Action {
+  readonly type = CARGAR_USUARIOS_SUCCESS;
+
+  constructor( public usuarios: UsuarioModel[] ) {}
+}
+
+// ToDo: 3. Exportar los tipos validos que las acciones van a poder emitir
+export type usuariosActions = CargarUsuarios | CargarUsuariosFail | CargarUsuariosSuccess;
